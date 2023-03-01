89c32ba57bf6b59e021532334d63504b27ba7218
Alen Valdez <alenkvaldeztch@gmail.com>
2023-02-28 18:24:23 -0800

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,33 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "alen";
+let lastName = "valdez";
+var age = 25;
 
 // Exercise 2
+firstName = "aleman";
+lastName = "valdeezy";
+age = 25;
 
 // Exercise 3
+let language = "Javascript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+// ex 4
+
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock  = 0;
+let inStock = false;
+let selectedSize = "M";
+
+// ex 5
+
+let tittle = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
